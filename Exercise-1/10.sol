a66122043331cc82261beaf34e03747a5460ce07
Rukundo <gihozoRukundobenise@gmail.com>
2026-03-17 07:26:26 +0200

    Question 10

---
+++ Exercise-1/10.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MathOperations {
+
+    // Function to add two integers
+    function add(int a, int b) public pure returns (int) {
+        return a + b;
+    }
+}
